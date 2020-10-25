e388680f1da6f4b3448c93fd61b3ac66f7b12a93
Clovis Chakrian <clovischakriandev@gmail.com>
2020-10-25 16:58:47 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [a, setA] = useState(Number);
+  const [b, setB] = useState(Number);
+  const [c, setC] = useState(Number);
+  const [x1, setX1] = useState(Number);
+  const [x2, setX2] = useState(Number)
+
+  function handleCalc() {
+    const delta = ((b) ** 2) - 4 * a * c;
+    const raiz = delta ** 0.5;
+    setX1(((-(b) + raiz) / (2 * a)));
+    setX2(((-(b) - raiz) / (2 * a)));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="top-bar">
+        <h1 className="title">Bhaskara Calc</h1>
       </header>
+
+      <main>
+        <form >
+          <label htmlFor="valueA" className='labels'>Digite o valor de A abaixo:</label>
+          <input onChange={e => setA(Number(e.target.value))} id="valueA" className="default-input" type="number" placeholder="Digite aqui o valor de A" />
+
+          <label htmlFor="valueB" className='labels'>Digite o valor de B abaixo:</label>
+          <input onChange={e => setB(Number(e.target.value))} id="valueB" className="default-input" type="number" placeholder="Digite aqui o valor de B" />
+
+          <label htmlFor="valueC" className='labels'>Digite o valor de C abaixo:</label>
+          <input onChange={e => setC(Number(e.target.value))} id="valueC" className="default-input" type="number" placeholder="Digite aqui o valor de C" />
+
+          <input onClick={handleCalc} type="button" value="Calcular" id="button-calc" />
+        </form>
+
+        <div id="result">
+          <p className="x">X'= {x1}</p>
+          <p className="x">X"= {x2}</p>
+        </div>
+      </main>
     </div>
   );
 }
